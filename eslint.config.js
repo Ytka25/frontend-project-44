3770e37731100d5ac8b592085c1d62249091daee
Ytka25 <ilya.G2102@mai.ru>
2026-04-19 03:11:50 +0300

    5

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,28 @@
 import stylistic from '@stylistic/eslint-plugin';
-import js from '@eslint/js';
-import globals from 'globals';
+
 
 export default [
-    js.configs.recommended,
-    {
-        files: ['**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.node, 
-            },
-        },
-        plugins: {
-            '@stylistic': stylistic, 
-        },
-        rules: {
-            '@stylistic/indent': ['error', 4],
-            '@stylistic/quotes': ['error', 'single'],
-            '@stylistic/semi': ['error', 'always'],
-        },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      '@stylistic': stylistic,
     },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+      },
+    },
+    rules: {
+      '@stylistic/indent': ['error', 4],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+    },
+  },
 ];
