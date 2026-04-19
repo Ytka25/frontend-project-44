1c5031cf474ff1b259f4581646c3d9afa8ecb224
Ytka25 <ilya.G2102@mai.ru>
2026-04-19 03:00:10 +0300

    cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,34 +1,9 @@
 import readlineSync from 'readline-sync';
 
-
-const isEven = (num) => num % 2 === 0;
-
-
-const playEvenGame = () => {
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const roundsCount = 3;
-
-  for (let i = 0; i < roundsCount; i += 1) {
-    const number = Math.floor(Math.random() * 100) + 1; 
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return; 
-    }
-
-    console.log('Correct!');
-  }
-
-  console.log(`Congratulations, ${name}!`);
 };
 
-export default playEvenGame;
+export default greetUser;
